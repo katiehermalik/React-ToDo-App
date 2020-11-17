a3dfe04f397b4484cc274e58f18d32d20667af10
khermalik <khermalik@gmail.com>
2020-11-17 14:21:58 -0800

    adds todo creation form and functionality

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TodoModel from '../models/Todo';
 import Todos from '../components/Todos';
+import CreateTodoForm from '../components/CreateTodoForm';
 
 class TodosContainer extends Component {
   constructor() {
@@ -22,14 +23,30 @@ class TodosContainer extends Component {
     });
   };
   
+  createTodo = (todo) => {
+    let newTodo = {
+        body: todo,
+        completed: false,
+    };
+    
+    TodoModel.create(newTodo).then((res) => {
+        let todos = this.state.todos;
+        todos.push(res.data);
+        this.setState({ todos: todos });
+    });
+  };
+
   render() {
     return (
       <div className="todosComponent">
+        <CreateTodoForm
+          createTodo={this.createTodo} />
+
         <Todos
           todos={this.state.todos} />
       </div>
     );
-  };
+  };    
 };
 
 export default TodosContainer;
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -3,9 +3,15 @@ import axios from 'axios';
 const endPoint = `https://super-crud.herokuapp.com/todos`;
 
 class TodoModel {
+  
   static all = () => {
     let request = axios.get(endPoint);
     // returning request so that we can chain promises to it in TodosContainer
+    return request;
+  };
+  
+  static create = (todo) => {
+    let request = axios.post(endPoint, todo);
     return request;
   };
 };
